105840b94629df97ea79d11cd3ec0cc0bd13084c
Mahesh Mohan <maheshmohan@Maheshs-MacBook-Air.local>
2022-11-05 12:27:24 +0530

    latest terraform code for infra provisioning

--- main.tf
+++ main.tf
@@ -9,44 +9,93 @@ resource "aws_ecr_repository" "ecr-repo-mahesh" {
 }
 
 resource "aws_ecs_cluster" "flaskapp-cluster" {
-  name = "flaskapp-cluster"
+  name = "flaskapp-cluster2"
 }
 
-resource "aws_ecs_task_definition" "flaskapp-task" {
-  family                   = "flaskapp-task"
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "flaskapp-img",
-      "image": "flaskapp-img",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 5000,
-          "hostPort": 5000
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = data.aws_iam_role.task_ecs.arn
-}
 
 resource "aws_ecs_service" "flaskapp-service" {
-  name            = "flaskapp-servic"                             # Naming our first service
+  name            = "flaskapp-servic2"                             # Naming our first service
   cluster         = "${aws_ecs_cluster.flaskapp-cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.flaskapp-task.id}" # Referencing the task our service will spin up
+  task_definition = flaskappfam-img3 #"${aws_ecs_task_definition.flaskapp-task.id}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 2 # Setting the number of containers we want to be deployed to 2
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.flaskapp-task.family}"
+    container_port   = 5000 # Specifying the container port
+  }
+
   network_configuration {
     subnets          = data.aws_subnet_ids.subnets.ids #Refer subnets from default vpc datasource
     assign_public_ip = true # Create public ip for containers
+  }
+}
+
+# Security group for ECS service to allow traffic from LB security group
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allow incoming traffic from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allow any incoming port
+    to_port     = 0 # Allow any outgoing port
+    protocol    = "-1" # Allow any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"] # Allow outgoing traffic to all IP
+  }
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "flaskapp-ecs-lb"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.subnets.ids #Refer subnets from default vpc datasource
+  # Referencing the LB security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Security group for the load balancer
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80 #Allow incoming traffic from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #Allow incoming traffic from everywhere
+  }
+
+  egress {
+    from_port   = 0 # Allow any incoming port
+    to_port     = 0 # Allow any outgoing port
+    protocol    = "-1" # Allow any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"] # Allow outgoing traffic to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "flaskapp-ecs-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${data.aws_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+    timeout = 110
+    interval = 120
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
   }
 }
 
